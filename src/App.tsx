ed5a7981215c71605e9ca2090535d961d6209a4a
Charile333 <charile333@example.com>
2026-01-04 18:46:15 +0800

    Remove settings, docs, and hash-lookup routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import MainLayout from './components/layout/MainLayout';
 import Dashboard from './pages/Dashboard';
 import DomainMonitor from './pages/DomainMonitor';
 import LeakQuery from './pages/LeakQuery';
-import Documentation from './pages/Documentation';
-import HashLookup from './pages/HashLookup';
-import Settings from './pages/Settings';
+
 
 function App() {
   return (
@@ -16,9 +14,7 @@ function App() {
           <Route path="/dns" element={<Dashboard />} />
           <Route path="/monitor" element={<DomainMonitor />} />
           <Route path="/leak-query" element={<LeakQuery />} />
-          <Route path="/hash-lookup" element={<HashLookup />} />
-          <Route path="/docs" element={<Documentation />} />
-          <Route path="/settings" element={<Settings />} />
+
           <Route path="/alerts" element={<div className="p-8 text-center text-gray-500">告警中心模块正在开发中...</div>} />
         </Routes>
       </MainLayout>
